084666919f17cae727685fb86ba9280866edcb61
Daniel Tsai <idaniel.twc@gmail.com>
2016-08-12 22:49:04 +0800

    lab members template and view, navtop and css

--- people/views.py
+++ people/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def members(request):
+    postdoc = models.Member.objects.filter(identity='Postdoc')
+    master = models.Member.objects.filter(identity='Master Student')
+    phd = models.Member.objects.filter(identity='PhD Student')
+    assistant = models.Member.objects.filter(identity='Assistant')
+
+    member_groups = [
+        {'title':'Postdoc', 'members':postdoc},
+        {'title':'PhD Student', 'members':phd},
+        {'title':'Master Student', 'members':master},
+        {'title':'Assistant', 'members':assistant},
+    ]
+
+
+    return render(request, 'members.html', locals())
